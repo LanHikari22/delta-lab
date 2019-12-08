81038533a7be3a3140f88e67aaaa4b3e4cb63e6f
Lan <lanhikarixx@gmail.com>
2019-12-07 23:11:21 -0600

    solve AOC::day2

--- advent-of-code/day2/src/intcode.rs
+++ advent-of-code/day2/src/intcode.rs
@@ -20,6 +20,7 @@ pub enum InstructionErrorCode {
 }
 
 
+#[derive(Debug, PartialEq)]
 pub enum TapeExecutionError {
     ParamPositionOutOfBounds {p1: bool, p2: bool, res: bool},
     OutOfBoundCalculation,
@@ -29,21 +30,21 @@ pub enum TapeExecutionError {
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum InstructionCode {
-    Add = 0,
-    Sub = 1,
+    Add = 1,
+    Mul = 2,
     End = 99,
 }
 
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Instruction {
-    Add {p1_pos: u8, p2_pos: u8, res_pos: u8, inst_size: usize},
-    Sub {p1_pos: u8, p2_pos: u8, res_pos: u8, inst_size: usize},
+    Add {p1_pos: i32, p2_pos: i32, res_pos: i32, inst_size: usize},
+    Mul {p1_pos: i32, p2_pos: i32, res_pos: i32, inst_size: usize},
     End {inst_size: usize},
 }
 
 
-pub fn read_tape(input_path: &str) -> Vec<u8> {
+pub fn read_tape(input_path: &str) -> Vec<i32> {
     let path = Path::new(input_path);
     // println!("{}", format!("file: {}", path.display()));
 
@@ -63,11 +64,11 @@ pub fn read_tape(input_path: &str) -> Vec<u8> {
         vec!['\n', ','].iter().any(|split_char| c == *split_char)
     }
 
-    let mut out: Vec<u8> = vec![];
+    let mut out: Vec<i32> = vec![];
     for token in content
             .split(is_split_char)
             .filter(|token| *token != "") {
-        let num = match token.parse::<u8>() {
+        let num = match token.parse::<i32>() {
             Err(why) => panic!("could not parse \"{}\": {}", token, why.description()),
             Ok(num) => num,
         };
@@ -81,8 +82,8 @@ pub fn read_tape(input_path: &str) -> Vec<u8> {
 }
 
 
-pub fn execute_tape(tape: &[u8]) -> Result<Vec<u8>, TapeExecutionError>{
-    let mut output_tape: Vec<u8> = tape.iter().map(|i: &u8| *i).collect();
+pub fn execute_tape(tape: &[i32]) -> Result<Vec<i32>, TapeExecutionError>{
+    let mut output_tape: Vec<i32> = tape.iter().map(|i: &i32| *i).collect();
 
     for (i, inst_res) in read_next_instruction(tape).enumerate() {
         let inst = match inst_res {
@@ -97,19 +98,19 @@ pub fn execute_tape(tape: &[u8]) -> Result<Vec<u8>, TapeExecutionError>{
 }
 
 
-pub fn read_instruction(tape: &[u8]) -> Result<Instruction, InstructionErrorCode> {
+pub fn read_instruction(tape: &[i32]) -> Result<Instruction, InstructionErrorCode> {
     // ensure at least one byte containing the instruction is available
     if tape.len() < 1 {
         return Err(InstructionErrorCode::NoInstructionFound);
     }
     
-    if tape[0] == InstructionCode::Add as u8 {
+    if tape[0] == InstructionCode::Add as i32 {
         if tape.len() < 4 { return Err(InstructionErrorCode::InvalidParamCount); }
         return Ok(Instruction::Add {p1_pos: tape[1], p2_pos: tape[2], res_pos: tape[3], inst_size: 4})
-    } else if tape[0] == InstructionCode::Sub as u8 {
+    } else if tape[0] == InstructionCode::Mul as i32 {
         if tape.len() < 4 { return Err(InstructionErrorCode::InvalidParamCount); }
-        return Ok(Instruction::Sub {p1_pos: tape[1], p2_pos: tape[2], res_pos: tape[3], inst_size: 4})
-    } else if tape[0] == InstructionCode::End as u8 {
+        return Ok(Instruction::Mul {p1_pos: tape[1], p2_pos: tape[2], res_pos: tape[3], inst_size: 4})
+    } else if tape[0] == InstructionCode::End as i32 {
         return Ok(Instruction::End {inst_size: 1})
     } else {
         return Err(InstructionErrorCode::InvalidInstruction);
@@ -117,8 +118,8 @@ pub fn read_instruction(tape: &[u8]) -> Result<Instruction, InstructionErrorCode
 }
 
 
-pub fn execute_instruction(inst: &Instruction, output_tape: &mut Vec<u8>) -> Result<(), TapeExecutionError> {
-    fn check_positions_in_bound(p1_pos: u8, p2_pos: u8, res_pos: u8, tape_length: usize) -> Result<(), TapeExecutionError> {
+pub fn execute_instruction(inst: &Instruction, output_tape: &mut Vec<i32>) -> Result<(), TapeExecutionError> {
+    fn check_positions_in_bound(p1_pos: i32, p2_pos: i32, res_pos: i32, tape_length: usize) -> Result<(), TapeExecutionError> {
         if [p1_pos, p2_pos, res_pos].iter().any(|&pos| pos as usize >= tape_length) {
             let v = [p1_pos, p2_pos, res_pos].iter().map(|&pos| pos as usize >= tape_length).collect::<Vec<bool>>();
             return Err(TapeExecutionError::ParamPositionOutOfBounds {p1: v[0], p2: v[1], res: v[2]});
@@ -133,12 +134,11 @@ pub fn execute_instruction(inst: &Instruction, output_tape: &mut Vec<u8>) -> Res
             let b = output_tape[*p2_pos as usize];
             output_tape[*res_pos as usize] = a + b;
         },
-        Instruction::Sub {p1_pos, p2_pos, res_pos, ..} => {
+        Instruction::Mul {p1_pos, p2_pos, res_pos, ..} => {
             check_positions_in_bound(*p1_pos, *p2_pos, *res_pos, output_tape.len())?;
             let a = output_tape[*p1_pos as usize];
             let b = output_tape[*p2_pos as usize];
-            if b > a {return Err(TapeExecutionError::OutOfBoundCalculation)}
-            output_tape[*res_pos as usize] = a - b;
+            output_tape[*res_pos as usize] = a * b;
         },
         _ => {},
     };
@@ -150,14 +150,14 @@ pub fn execute_instruction(inst: &Instruction, output_tape: &mut Vec<u8>) -> Res
 pub fn get_instruction_size(inst: &Instruction) -> usize {
     match inst {
         Instruction::Add {inst_size: s, ..} => *s,
-        Instruction::Sub {inst_size: s, ..} => *s,
+        Instruction::Mul {inst_size: s, ..} => *s,
         Instruction::End {inst_size: s, ..} => *s,
     }
 }
 
 
 // creates a generator for use in a foreach loop to read the next instruction
-fn read_next_instruction<'a>(tape: &'a [u8]) -> generator::Generator<'a, (), Result<Instruction, InstructionErrorCode>> {
+pub fn read_next_instruction<'a>(tape: &'a [i32]) -> generator::Generator<'a, (), Result<Instruction, InstructionErrorCode>> {
     Gn::new_scoped(move |mut s| { // must explicitly move to change capture environment for closure to use arguments
         let mut cur: usize = 0;
         loop {
@@ -189,36 +189,36 @@ mod tests {
         use super::*;
 
         pub struct TestData {
-            pub tape: Vec<u8>,
+            pub tape: Vec<i32>,
             pub instructions: Vec<Result<Instruction, InstructionErrorCode>>,
-            pub output_tape: Vec<u8>,
+            pub output_tape: Vec<i32>,
         }
 
         pub fn get_case_typical() -> TestData {
             let tape = vec![
-                InstructionCode::Add as u8, 9u8, 10u8, 11u8, 
-                InstructionCode::Sub as u8, 12u8, 13u8, 14u8,
-                InstructionCode::End as u8, 
-                1u8, 2u8, 0u8, 5u8, 3u8, 0u8
+                InstructionCode::Add as i32, 9i32, 10i32, 11i32, 
+                InstructionCode::Mul as i32, 12i32, 13i32, 14i32,
+                InstructionCode::End as i32, 
+                1i32, 2i32, 0i32, 5i32, 3i32, 0i32
             ];
             let instructions = vec![
-                Ok(Instruction::Add {p1_pos: 9u8, p2_pos: 10u8, res_pos: 11u8, inst_size: 4}),
-                Ok(Instruction::Sub {p1_pos: 12u8, p2_pos: 13u8, res_pos: 14u8, inst_size: 4}),    
+                Ok(Instruction::Add {p1_pos: 9i32, p2_pos: 10i32, res_pos: 11i32, inst_size: 4}),
+                Ok(Instruction::Mul {p1_pos: 12i32, p2_pos: 13i32, res_pos: 14i32, inst_size: 4}),    
                 Ok(Instruction::End {inst_size: 1}),
             ];
             let mut output_tape = tape.clone();
-            output_tape[11] = 3u8;
-            output_tape[14] = 2u8;
+            output_tape[11] = 3i32;
+            output_tape[14] = 15i32;
             
             return TestData {tape, instructions, output_tape};
         }
 
         pub fn get_case_early_end() -> TestData {
             let tape = vec![
-                InstructionCode::End as u8, 
-                InstructionCode::Add as u8, 9u8, 10u8, 11u8, 
-                InstructionCode::Sub as u8, 12u8, 13u8, 14u8,
-                0u8, 0u8, 0u8, 0u8, 0u8, 0u8
+                InstructionCode::End as i32, 
+                InstructionCode::Add as i32, 9i32, 10i32, 11i32, 
+                InstructionCode::Mul as i32, 12i32, 13i32, 14i32,
+                0i32, 0i32, 0i32, 0i32, 0i32, 0i32
             ];
             let instructions = vec![
                 Ok(Instruction::End {inst_size: 1}),
@@ -235,15 +235,15 @@ mod tests {
 
     #[test]
     fn test_read_instruction() {
-        assert_eq!(read_instruction(&[InstructionCode::Add as u8, 10, 11, 12]).unwrap(),
+        assert_eq!(read_instruction(&[InstructionCode::Add as i32, 10, 11, 12]).unwrap(),
                    Instruction::Add {p1_pos: 10, p2_pos: 11, res_pos: 12, inst_size: 4});
         
-        assert_eq!(read_instruction(&[InstructionCode::End as u8, 10, 11, 12]).unwrap(),
+        assert_eq!(read_instruction(&[InstructionCode::End as i32, 10, 11, 12]).unwrap(),
                    Instruction::End {inst_size: 1});
         
         assert_eq!(read_instruction(&[]), Err(InstructionErrorCode::NoInstructionFound));
         
-        assert_eq!(read_instruction(&[InstructionCode::Add as u8, 10, 11]),
+        assert_eq!(read_instruction(&[InstructionCode::Add as i32, 10, 11]),
                    Err(InstructionErrorCode::InvalidParamCount));
 
         assert_eq!(read_instruction(&[200]), Err(InstructionErrorCode::InvalidInstruction));
@@ -251,7 +251,7 @@ mod tests {
 
     #[test]
     fn test_read_next_instruction() {
-        fn run_test(tape: &[u8], instructions: &[Result<Instruction, InstructionErrorCode>]) {
+        fn run_test(tape: &[i32], instructions: &[Result<Instruction, InstructionErrorCode>]) {
             let act_instructions = read_next_instruction(tape).collect::<Vec<Result<Instruction, InstructionErrorCode>>>();
 
             assert_eq!(act_instructions.len(), instructions.len(), "size of instructions does not match");
--- advent-of-code/day2/src/main.rs
+++ advent-of-code/day2/src/main.rs
@@ -2,6 +2,7 @@
 extern crate generator;
 use generator::Gn;
 
+
 fn next_mult_of(n: u32) -> generator::Generator<'static, (), u32> {
     Gn::new_scoped(move |mut s| { // must explicitly move to change capture environment for closure and allow n to be used
         for a in 0.. {
@@ -15,10 +16,98 @@ fn next_mult_of(n: u32) -> generator::Generator<'static, (), u32> {
 
 mod intcode;
 const INPUT_TAPE: &str = "tape.in";
-fn main() {
-    intcode::read_tape(INPUT_TAPE);
 
-    let inst = intcode::read_instruction(&[1,0,0,1]);
-    println!("{:?}", inst);
+fn usage_exit() {
+    use std::process;
+    println!("usage: prog p1 p2");
+    println!("  where:");
+    println!("      p1: The value in position 1 in the program to modify");
+    println!("      p2: The value in position 2 in the program to modify");
+    std::process::exit(0);
+
+}
+
+fn run_and_display_program(input_tape: &[i32]) {
+    let output_tape: Vec<i32> = match intcode::execute_tape(&input_tape) {
+        Err(code) => panic!("failed to execute tape: {:?}", code),
+        Ok(output_tape) => output_tape,
+    };
+
+    println!("program:");
+    for inst_res in intcode::read_next_instruction(&input_tape) {
+        let inst = inst_res.unwrap();
+        println!("{:?}", inst);
+    }
+
+    println!("{:?}", output_tape);
+}
+
+
+fn main_experiment_with_program() { 
+    use std::env;
+    use std::process;
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 3 {
+        usage_exit();
+    }
+    let mut input_tape: Vec<i32> = intcode::read_tape(INPUT_TAPE);
+    input_tape[1] = args[1].parse::<i32>().unwrap();
+    input_tape[2] = args[2].parse::<i32>().unwrap();
+    run_and_display_program(&input_tape);
+}
+
+
+fn run_program(input_tape: &mut Vec<i32>, p1: i32, p2: i32) -> i32 {
+    input_tape[1] = p1;
+    input_tape[2] = p2;
+    
+    let output_tape: Vec<i32> = match intcode::execute_tape(&input_tape) {
+        Err(code) => panic!("failed to execute tape: {:?}", code),
+        Ok(output_tape) => output_tape,
+    };
+
+    output_tape[0]
+}
+
+
+fn calc_parameters(input_tape: &mut Vec<i32>, output_value: i32) -> Result<(i32, i32), (i32, i32)> {
+    // Program logic:
+    // F(a, b) = A + B*a + b
+    //  where
+    //      A = prog(0, 0)
+    //      B = prog(1, 0) - A
+
+    let A = run_program(input_tape, 0, 0);
+    let B = run_program(input_tape, 1, 0) - A;
+    println!("A: {}, B: {}", A, B);
+
+    // given F(a, b) = C => A + B*a + b = C:
+    //  a = (C-A) / B
+    //  b = (C-A) % B
+    let C = output_value;
+    let a = (C-A) / B;
+    let b = (C-A) % B;
+   
+    let out_of_bounds = |x: i32| x < 0 || x > 99;
+
+    if out_of_bounds(a) || out_of_bounds(b) {
+        return Err((a, b))
+    }
+
+    Ok((a,b))
+}
+
+fn main() {
+    use std::env;
+    use std::process;
+    let args = std::env::args().collect::<Vec<String>>();
+    
+    let mut input_tape: Vec<i32> = intcode::read_tape(INPUT_TAPE);
+
+    let C = 19690720;
+    match calc_parameters(&mut input_tape, C) {
+        Err(tup) => println!("error: invalid configuration: a: {}, b: {}", tup.0, tup.1),
+        Ok(tup) => println!("{}: a: {}, b: {}", tup.0 * 100 + tup.1, tup.0, tup.1),
+    };
 
 }
